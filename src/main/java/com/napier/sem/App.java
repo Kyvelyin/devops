d94e986085c8a26b89bdb6262af30a13a59280fe
Kyvelyin <tharshoownpyae@gmail.com>
2025-09-22 16:46:13 +0630

    Chang Connection method.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,9 +2,28 @@ package com.napier.sem;
 
 import java.sql.*;
 
-public class App
-{
+public class App {
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
     public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -17,9 +36,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -34,14 +51,11 @@ public class App
                         "example"
                 );
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
@@ -49,13 +63,20 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
             {
                 // Close connection
                 con.close();
+                System.out.println("Disconnected from database.");
             }
             catch (Exception e)
             {
@@ -63,4 +84,6 @@ public class App
             }
         }
     }
+
+
 }
